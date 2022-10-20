ad7f47ebf61a2c902c438c97937055a26d0a4c05
TechTalkerShubh <jitiya66@gmail.com>
2022-10-20 13:38:35 +0530

    Theme (dark + light)

--- app/src/main/java/com/shubhamjitiya/mad/practicals/Practical_7.java
+++ app/src/main/java/com/shubhamjitiya/mad/practicals/Practical_7.java
@@ -1,10 +1,13 @@
 package com.shubhamjitiya.mad.practicals;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.shubhamjitiya.mad.R;
 import com.shubhamjitiya.mad.adapter.StudentDetailsAdapter;
@@ -26,5 +29,30 @@ public class Practical_7 extends AppCompatActivity {
         binding.rvPractical7.setHasFixedSize(true);
         binding.rvPractical7.setLayoutManager(new LinearLayoutManager(this));
         binding.rvPractical7.setAdapter(studentAdapter);
+
+        binding.btnTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nightModeFlags =
+                        view.getContext().getResources().getConfiguration().uiMode &
+                                Configuration.UI_MODE_NIGHT_MASK;
+                switch (nightModeFlags) {
+                    case Configuration.UI_MODE_NIGHT_YES:
+                         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        Toast.makeText(Practical_7.this, "Night mode disabled", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case Configuration.UI_MODE_NIGHT_NO:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        Toast.makeText(Practical_7.this, "Night mode enabled", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                        Toast.makeText(Practical_7.this, "DK ch", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+            }
+        });
     }
 }
