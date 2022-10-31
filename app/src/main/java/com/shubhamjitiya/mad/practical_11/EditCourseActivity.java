ccb6765039641936f2221344f7ba731622abbc8a
TechTalkerShubh <jitiya66@gmail.com>
2022-10-31 16:03:08 +0530

    Setup Project

---
+++ app/src/main/java/com/shubhamjitiya/mad/practical_11/EditCourseActivity.java
@@ -0,0 +1,16 @@
+package com.shubhamjitiya.mad.practical_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shubhamjitiya.mad.R;
+
+public class EditCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_course);
+    }
+}
